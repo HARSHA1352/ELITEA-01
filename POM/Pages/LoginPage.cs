5e5feafa9c7454055ebfe05722d2611421329151
Om Sree Harsha <91528670+HARSHA1352@users.noreply.github.com>
2024-11-13 15:09:56 +0530

    Update POM/Pages/LoginPage.cs

--- POM/Pages/LoginPage.cs
+++ POM/Pages/LoginPage.cs
@@ -9,10 +9,17 @@ namespace POM.Pages
 {
     public class LoginPage
     {
-        private By Username => By.Id("user-name");
-        private By Password => By.Id("password");
-        private By LoginButton => By.Id("login-button");
-        private By ErrorContainer => By.CssSelector(".error-message-container");
+        // Method to locate username field
+        public IWebElement UsernameField => _driver.FindElement(Username);
+
+        // Method to locate password field
+        public IWebElement PasswordField => _driver.FindElement(Password);
+
+        // Method to locate login button
+        public IWebElement LoginButton => _driver.FindElement(LoginButton);
+
+        // Method to locate error message container
+        public IWebElement ErrorContainer => _driver.FindElement(ErrorContainer);
         
     }
 }
