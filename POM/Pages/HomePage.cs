0f950d4cef9df5fb6a73e463c5abf099a98cb769
Om Sree Harsha <91528670+HARSHA1352@users.noreply.github.com>
2024-11-13 15:10:05 +0530

    Create POM/Pages/HomePage.cs

---
+++ POM/Pages/HomePage.cs
@@ -0,0 +1,13 @@
+using OpenQA.Selenium;
+
+namespace POM.Pages
+{
+    public class HomePage : BasePage
+    {
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        // Add methods for interacting with elements on the home page
+    }
+}
